345043533d698757584a22bd4be4088e70869c83
gilmar123-san <69655786+gilmar123-san@users.noreply.github.com>
2023-07-18 07:16:49 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+
+public class SellerDaoJDBC implements SellerDao{
+
+	@Override
+	public void insert(model.entities.Seller obj) {		
+		
+	}
+
+	@Override
+	public void Seller(model.entities.Seller obj) {		
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {		
+		
+	}
+
+	@Override
+	public model.entities.Seller findById(Integer id) {		
+		return null;
+	}
+
+	@Override
+	public List<model.entities.Seller> findAll() {		
+		return null;
+	}
+
+}
