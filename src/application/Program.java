83f9f03a83e1bdec93178a2fa08db5edd5a71267
gilmar123-san <69655786+gilmar123-san@users.noreply.github.com>
2023-07-20 06:44:45 -0300

    insert implementationinsert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -27,6 +28,11 @@ public class Program {
 		System.out.println("\n=== TEST 3: seller findAll ===");		
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);
+		
+		System.out.println("\n=== TEST 4: seller insert ===");		
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id = " + newSeller.getId());
 	}
 
 }
