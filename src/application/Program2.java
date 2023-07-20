f2b0921b402a81829d5ae41207a57c622a8938c4
gilmar123-san <69655786+gilmar123-san@users.noreply.github.com>
2023-07-20 07:42:18 -0300

    Create DepartmentDaoJDBC and some your methods, how findById and findAll

---
+++ src/application/Program2.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+	
+	public static void main(String[] args) {
+		
+		DepartmentDao depDao = new DaoFactory().createDepartmentDao();
+		
+		System.out.println("=== TEST 1: department findByID ===");
+		Department dep = depDao.findById(1);
+		System.out.println(dep);
+		
+		System.out.println("\n=== TEST 2: department findAll ===");
+		
+		List<Department> list = depDao.findAll();
+		list.forEach(System.out::println);
+	}
+}
